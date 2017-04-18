7fd1a1a5118367f95a9304f754671ecf85914437
Miles Maddox <justmiles@users.noreply.github.com>
2017-04-17 21:09:12 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ if ! grep -q $GROUP_ID /etc/group; then groupadd -g $GROUP_ID qbittorrent; fi
 
 if ! grep -q $USER_ID /etc/passwd; then useradd --shell /bin/bash -u $USER_ID -g $GROUP_ID -o -c "" -m qbittorrent; fi
 
-chown -R qbittorrent.qbittorrent /config /torrents /downloads
+# chown -R qbittorrent.qbittorrent /config /torrents /downloads
 
 mkdir -p /home/qbittorrent/.config
 
